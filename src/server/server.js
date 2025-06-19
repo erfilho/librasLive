48bb2ef16def7c7bb9beb5ee57db8b71d81961a8
Erineldo Filho <erineldot@gmail.com>
2025-06-19 17:02:33 -0300

    #23 - Added the API Endpoint for audio transcription¨

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,12 @@
-// server.js
+import dotenv from "dotenv";
+
 import express from "express";
 import cors from "cors";
+
+import fs from "fs";
+import multer from "multer";
+
 import OpenAI from "openai";
-import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -10,10 +14,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const upload = multer({ dest: "/uploads" });
+
 const openai = new OpenAI({
   apiKey: process.env.API_KEY,
 });
 
+// Endpoint for translation of transcribe for best mimes for Libras using the openAI API
 app.post("/translate", async (req, res) => {
   const { text } = req.body;
 
@@ -35,12 +42,33 @@ app.post("/translate", async (req, res) => {
 
     res.json({ translation: response.choices[0].message.content });
   } catch (err) {
-    console.error("Erro na API do OpenAI:", err);
+    console.error("Erro na tradução para Libras: ", err);
     res.status(500).json({ error: "Erro ao traduzir" });
   }
 });
 
+// Endpoint for transcribe the audio for text with openAi API
+app.post("/transcribe", upload.single("audio"), async (req, res) => {
+  const audioPath = req.file.path;
+
+  try {
+    const transcription = await openai.audio.transcriptions.create({
+      file: fs.createReadStream(audioPath),
+      model: "whisper-1",
+      language: "pt",
+    });
+
+    res.join({ transcription: transcription.text });
+  } catch (err) {
+    console.error("Erro na transcrição de áudio: ", err);
+    res.status(500).json({ error: "Erro ao transcrever" });
+  } finally {
+    fs.unlinkSync(audioPath);
+  }
+});
+
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
